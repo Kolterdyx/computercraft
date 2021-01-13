750787f1e4d93baff0c523cbb6cf28c59821b630
Kolterdyx <ciruelo@gmail.com>
2021-01-13 16:21:51 +0100

    duh

--- putosSA/computer/home_system/startup.lua
+++ putosSA/computer/home_system/startup.lua
@@ -1,2 +1,3 @@
+shell.run("rm", "update.lua")
 shell.run("wget", "https://raw.githubusercontent.com/Kolterdyx/computercraft/master/putosSA/computer/home_system/update.lua")
 shell.run("home")
--- putosSA/computer/home_system/update.lua
+++ putosSA/computer/home_system/update.lua
@@ -1,19 +1,18 @@
+print("Removing old home.lua")
+shell.run("rm", "home.lua")
 print("Updating home.lua")
-if fs.exists("home.lua") then
-    print("Removing old home.lua")
-    fs.delete("home.lua")
-end
 shell.run("wget", "https://raw.githubusercontent.com/Kolterdyx/computercraft/master/putosSA/computer/home_system/home.lua")
-print("Making sure table_saver.lua exists")
-if not fs.exists("table_saver.lua") then
-    shell.run("wget", "https://raw.githubusercontent.com/Kolterdyx/computercraft/master/putosSA/computer/home_system/table_saver.lua")
-end
+
+
+print("Updating table_saver.lua")
+shell.run("rm", "table_saver.lua")
+print("Updating table_saver.lua")
+shell.run("wget", "https://raw.githubusercontent.com/Kolterdyx/computercraft/master/putosSA/computer/home_system/table_saver.lua")
+
+print("Removing old startup.lua")
+shell.run("rm", "startup.lua")
 print("Updating startup.lua")
-if fs.exists("startup.lua") then
-    print("Removing old startup.lua")
-    fs.delete("startup.lua")
-end
 shell.run("wget", "https://raw.githubusercontent.com/Kolterdyx/computercraft/master/putosSA/computer/home_system/startup.lua")
 
 print("rebooting")
-os.reboot()
+--os.reboot()
