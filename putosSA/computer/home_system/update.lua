ae48b6ea20a3bef993959726a8f53ab2ab7c6d54
Kolterdyx <ciruelo@gmail.com>
2021-01-13 16:10:23 +0100

    Added logging

--- putosSA/computer/home_system/update.lua
+++ putosSA/computer/home_system/update.lua
@@ -1,14 +1,17 @@
+print("Updating home.lua")
 if fs.exists("home.lua") then
     fs.delete("home.lua")
 end
 shell.run("wget", "https://raw.githubusercontent.com/Kolterdyx/computercraft/master/putosSA/computer/home_system/home.lua")
+print("Making sure table_saver.lua exists")
 if not fs.exists("table_saver.lua") then
     shell.run("wget", "https://raw.githubusercontent.com/Kolterdyx/computercraft/master/putosSA/computer/home_system/table_saver.lua")
 end
+print("Updating startup.lua")
 if fs.exists("startup.lua") then
     fs.delete("startup.lua")
 end
 shell.run("wget", "https://raw.githubusercontent.com/Kolterdyx/computercraft/master/putosSA/computer/home_system/startup.lua")
 
-
+print("rebooting")
 os.reboot()
